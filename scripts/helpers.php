46c46e6efe22f4c486b5b4b78ef6dd280247b403
Ed Grosvenor <ed.grosvenor@wickedviral.com>
2020-05-26 11:06:33 -0500

    WIP

--- scripts/helpers.php
+++ scripts/helpers.php
@@ -1,8 +1,13 @@
 <?php
 
+use Illuminate\Support\Str;
+
 if (!function_exists('cloudinary')) {
     function cloudinary($source, $transformation, $args = [])
     {
+        if ($transformation === 'hero' && !Str::contains($source, '.hei')) {
+            $transformation = 'hero_raw';
+        }
         $transform = (new \Grosv\LaravelCloudinary\Transform())->$transformation();
         return (new \Grosv\LaravelCloudinary\Client)->url($source, $transform);
     }
